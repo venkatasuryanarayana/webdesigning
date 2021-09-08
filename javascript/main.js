87be45ab0fffb0c496b682a7225d05f2dc264a13
venkatasuryanarayana <32256493+venkatasuryanarayana@users.noreply.github.com>
2021-09-08 12:54:10 +0530

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -12,11 +12,66 @@
 // console.log(student.id+" "+student.name+" "+student.branch);
 
 
-function student(id,name,branch) {
-this.id=id;
-this.name=name;
-this.branch=branch;
-}
+// function student(id,name,branch) {
+// this.id=id;
+// this.name=name;
+// this.branch=branch;
+// }
+//
+// var s = new student(540,"srikanth","EEE");
+// document.writeln(s);
 
-var s = new student(540,"srikanth","EEE");
-document.writeln(s);
+// var student = ["venkata","surya","narayana","narla"];
+// document.writeln(student.length);
+// for(i=0;i<student.length;i++){
+//   document.writeln(student[i]);
+// }
+
+// var student = new Array();
+// student[0]="rajendra";
+// student[1]="banu";
+// student[2]="srikanth";
+//
+// for(i=0;i<student.length;i++){
+//   document.writeln(student[i]);
+// }
+
+
+// var student = new Array("veera","venkata","narayana");
+//
+// for(i=0;i<student.length;i++){
+//   document.writeln(student[i]);
+// }
+
+//charAt : It returns the character at given index.
+// var str = "javascript";
+// document.writeln(str.charAt(30));
+
+//concat: joins the two strings
+// var s1 = "surya";
+// var s2 = "345";
+// var s3 = s1.concat(s2);
+// var s4 = s3.concat("veera");
+// document.writeln(s4);
+
+// indexof: It returns the index position of the string.
+// var s1 = "hello hai everyone hai";
+// var s2 = s1.lastIndexOf("hai");
+// document.writeln(s2);
+
+// var s1 = "Hai hOw ArE You";
+// var s2 = s1.toLowerCase();
+// document.writeln(s2);
+
+// var s1 = "surya";
+// var s2 = s1.slice(1,4);
+// document.writeln(s2);
+
+// var s1 = "   surya narayana   ";
+// var s2 = s1.trim();
+// document.writeln(s2);
+
+// var str = "This is venkat";
+// var str2 = str.split(" ");
+// document.writeln(str2);
+// document.writeln(typeOf(str2));
